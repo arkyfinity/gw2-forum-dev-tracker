74f9c06375d27f82f520878264479cf422f766fe
Arkyfinity <hello@arky.dk>
2022-07-28 08:39:50 +0200

    Moved url to variable

--- helpers/discordlog.js
+++ helpers/discordlog.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 export const discordlog = (file, postmessage) => {
     axios({
         method: 'POST',
-        url: 'https://discord.com/api/webhooks/794541748793507851/FakL6x7d3Hvi2NZW6abAl4aNt-5iaDER_azF1_xq3X_KwBAZLs-l053ZHbctKpf104q9',
+        url: process.env.discordlog,
         headers: {
             'Content-Type': 'application/json'
         },
